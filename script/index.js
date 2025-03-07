57e1f8759b301def004c80ab85dd23ce02bd2e9c
www.ming115 <www.ming115@gmail.com>
2025-03-07 11:15:00 +0900

    nav 동작 추가

--- script/index.js
+++ script/index.js
@@ -4,13 +4,30 @@ const noticeClose = document.querySelector('.hd_top #close_btn')
 noticeClose.addEventListener('click',()=>{
     headerNotice.style.display = 'none';
 })
-/* scroll에 따른 nav 색상 변환 */
+/* scroll에 따른 nav 배경색 변환 */
 const navBar = document.querySelector('header .hd_navbar')
-navBar.classList.remove('navbar_shadow')
 window.addEventListener('scroll',()=>{
-    if ( window.scrollY > 800) {
-        navBar.classList.add('navbar_shadow')
-    } else { navBar.classList.remove('navbar_shadow') }
+    if (window.scrollY > 800) {
+        navBar.classList.add('active')
+    } else {navBar.classList.remove('active')}
+})
+/* nav에 hover 시 배경색 변환 */
+navBar.addEventListener('mouseover',()=>{
+    navBar.style.cssText='background-color: #fff; box-shadow: rgba(50, 50, 105, 0.15) 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px; transition: all 0.8s;'
+})
+navBar.addEventListener('mouseout',()=>{
+    navBar.style.cssText='background-color: none; box-shadow: none; transition: all 0.6s;'
+})
+/* nav 메뉴에 hover 시 메뉴선 활성화 */
+const navMenu = document.querySelectorAll('header nav .path li');
+console.log(navMenu)
+navMenu.forEach((menu)=>{
+    menu.addEventListener('mouseover',()=>{
+        menu.classList.add('active')
+    })
+    menu.addEventListener('mouseout',()=>{
+        menu.classList.remove('active')
+    })
 })
 /* (메인 배너) 제품 더보기 버튼 오버 시, 슬라이드 정지 */
 const bnrMoreBtn = document.querySelectorAll('.main_bnr_wrap .main_bnr #bnr_more')
@@ -24,7 +41,6 @@ bnrMoreBtn.forEach((btn) => {
 })
 /* 콜렉션 상품 카테고리 활성화 */
 const lineCategory = document.querySelectorAll('.main_wrap .line_style_category li')
-console.log(lineCategory)
 lineCategory.forEach((list) => {
     list.addEventListener('mouseover', ()=>{
         list.classList.add('hover');
